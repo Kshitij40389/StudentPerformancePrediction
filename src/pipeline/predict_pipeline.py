ed8ad095fbf58c0a8695cc87536892b39d4de2d6
Kshitij40389 <kshitijdatar04@gmail.com>
2025-02-12 13:27:15 +0530

    Test

--- src/pipeline/predict_pipeline.py
+++ src/pipeline/predict_pipeline.py
@@ -3,47 +3,35 @@ import pandas as pd
 from src.exception import CustomException
 from src.utils import load_object
 
+
 class PredictPipeline:
     def __init__(self):
         pass
 
-    def predict(self, features):
+    def predict(self,features):
         try:
-            # Define paths to model and preprocessor
-            model_path = 'artifacts\\model.pkl'
-            preprocessor_path = 'artifacts\\proprocessor.pkl'
-            
-            # Load model and preprocessor from disk
+            model_path ='artifacts\model.pkl'
+            preprocessor_path = 'artifacts\proprocessor.pkl'
             model = load_object(file_path=model_path)
             preprocessor = load_object(file_path=preprocessor_path)
-
-            # Fill missing values (None or NaN) with a default value for categorical features
-            features = features.fillna({'gender': 'unknown', 'Area': 'unknown', 'type': 'unknown'})
-
-            # Transform features using preprocessor
-            data_scaled = preprocessor.transform(features)
-            
-            # Predict using the loaded model
+            data_scaled=preprocessor.transform(features)
             preds = model.predict(data_scaled)
-            
             return preds
 
         except Exception as e:
-            # Raise custom exception in case of errors
             raise CustomException(e, sys)
 
+
 class CustomData:
-    def __init__(self, gender:str, Area:str, type:str, verbal:int, logical:int):
-        # Handle missing categorical values by assigning a default value
-        self.gender = gender if gender is not None else "unknown"
-        self.Area = Area if Area is not None else "unknown"
-        self.type = type if type is not None else "unknown"
+    def __init__(self,gender:str, Area:str, type:str, verbal:int, logical:int):
+        self.gender = gender
+        self.Area = Area
+        self.type = type
         self.verbal = verbal
         self.logical = logical
 
     def get_data_as_data_frame(self):
         try:
-            # Create DataFrame from input data
             custom_data_input_dict = {
                 "gender": [self.gender],
                 "Area": [self.Area],
@@ -54,5 +42,4 @@ class CustomData:
             return pd.DataFrame(custom_data_input_dict)
         
         except Exception as e:
-            # Raise custom exception in case of errors
             raise CustomException(e, sys)
